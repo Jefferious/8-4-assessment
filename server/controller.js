886238cfbf8df41d726a1571349b1a3f910b80c2
Jeffery Whittington <Jefferious@gmail.com>
2023-08-04 13:57:41 -0600

    Finished the fortune part of the assessment

---
+++ server/controller.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["A gambler not only will lose what he has, but also will lose what he doesn't have.", "A hunch is creativity trying to tell you something.", "A small donation is call for. It's the right thing to do.", "Advice, when most needed, is least heeded.", "All will go well with your new project."];
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
